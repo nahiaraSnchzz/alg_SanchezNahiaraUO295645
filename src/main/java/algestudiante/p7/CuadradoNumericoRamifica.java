c7024afb23d8c690d72b44501cad102872e43e05
uo295645 <uo295645@EII-L11-08.aulasuo.uniovi.es>
2024-04-25 20:03:17 +0200

    Se continua con la practica 7

--- src/main/java/algestudiante/p7/CuadradoNumericoRamifica.java
+++ src/main/java/algestudiante/p7/CuadradoNumericoRamifica.java
@@ -15,13 +15,15 @@ public class CuadradoNumericoRamifica extends Estado
 {
 	// Datos comunes a todos los estados, por eso los declaramos "static"
 	private static int n;				// Tama�o del problema, n�m. agentes y n�m. tareas
-
 	// Estado
 	private String[][] table;				
-
+	
+	private int x,y;
 	/**
 	 * Constructor para crear el estado inicial,
 	 * Establecemos los datos y condiciones iniciales del problema
+	 * 
+	 * Sirve para crear el estado inicial
 	 */
 	public CuadradoNumericoRamifica(String[][] table) {
 		super();
@@ -36,37 +38,39 @@ public class CuadradoNumericoRamifica extends Estado
 		}
 		
 
-		// Inicializa variables de cada estado
-		solParcial = new int[n];
-		for (int i=0;i<solParcial.length;i++)
-			solParcial[i] = -1;//sin asignaciones inicialmente
+		// Inicializa variables que usabamos en backtracking (si hay)
 		
-		tareaConTrabajador=new boolean[solParcial.length];
-		for (int i= 0; i<n; i++)
-			tareaConTrabajador[i]= false;
 		
 	}
 
 	/**
 	 * Constructor para crear un estado a partir del padre
+	 * 
+	 * Es el utilizado en el metodo expandir.
 	 */
 	public CuadradoNumericoRamifica(CuadradoNumericoRamifica padre,int j) {
 		super();
-		// copiamos toda la informacion del padre		
-		solParcial= Arrays.copyOf(padre.solParcial,n);
-		tareaConTrabajador= Arrays.copyOf(padre.tareaConTrabajador,n);
-		profundidad= padre.profundidad;
-		idPadre= padre.getId();
+		// copiamos toda la informacion del padre	
 		
-		// ESTAS DOS LINEAS CAMBIAN (estara el dame la siguiente casilla a evaluar)
-		// añade lo correspondiente a este hijo (le asigna el nuevo valor, j)
-		solParcial[profundidad] =j;		// profundidad se corresponde con el primer agente sin asignar
-		tareaConTrabajador[j]= true;	// marcamos la tarea como asignada
+		profundidad = padre.profundidad;
+		idPadre = padre.getId();
 		
+		// añade lo correspondiente a este hijo
+		// rellenar la siguiente celda del cuadado numerico
 		
+		int[] pos = siguienteCeldaParaRellenar(padre.x, padre.y); // posicion que relleno el padre
+		x = pos[0]; 
+		y = pos[1]; // posicion que tiene que rellenar el hijo
+		
+		table[x][y] = ""+j;
 		profundidad++;
 		calcularValorHeuristico();
 		
+	}
+
+	private int[] siguienteCeldaParaRellenar(int x2, int y2) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	/**
@@ -75,9 +79,7 @@ public class CuadradoNumericoRamifica extends Estado
 	 */
 	@Override
 	public void calcularValorHeuristico() {
-		valorHeuristico = 10;
-		
-			
+		valorHeuristico = 10;		
 	}
 
 	/**
@@ -87,57 +89,37 @@ public class CuadradoNumericoRamifica extends Estado
 	 * @return				valor m�nimo por columnas
 	 */
 	private int minimoColumna(int profundidad, int j) {
-		int minValorColumna=Integer.MAX_VALUE;
-		for (int i= profundidad; i<n; i++)
-			// calculamos el m�nimo de columnas
-			if (costes[i][j] < minValorColumna)
-				minValorColumna= costes[i][j];
-		return minValorColumna;
+//		int minValorColumna=Integer.MAX_VALUE;
+//		for (int i= profundidad; i<n; i++)
+//			// calculamos el m�nimo de columnas
+//			if (costes[i][j] < minValorColumna)
+//				minValorColumna= costes[i][j];
+//		return minValorColumna;
 	}
 
 	@Override
 	public ArrayList<Estado> expandir() {
 		ArrayList<Estado> listaHijos= new ArrayList<Estado>();
 		
-		int fila = -1;
-		int columna = -1;
-		
-		for (int i=0; i < table.length; i++	) {
-			for (int j=0; j < table[i].length; j++) {
-				if (table[i][j].equals("?")) {
-					fila = i;
-					columna = j;
-					break;
+		for (int j=0; j < 10; j++) {
+			Estado estadoHijo = new CuadradoNumericoRamifica(this,j);
+			if (estadoHijo.isValid()) {
+				listaHijos.add(estadoHijo);
+				
+				if (RamificaYPoda.TRAZA) {
+					System.out.println("Nivel: "+ profundidad + " - Hijo: " + j);
+					System.out.println(estadoHijo);
 				}
-			}
-			if (columna != -1) {
-				break;
 			}
 		}
 		
-		if (fila == -1 && columna == -1) {
-			return listaHijos;
-		}
-
-		
-		for (int numero = 0; numero < 10; numero++)
-			if (esSolucion(fila,columna))  // Crea hijo s�lo para tareas NO asignadas
-			{
-				if (comprobarColumnas() && comprobarFilas()) {
-					Estado estadoHijo= new CuadradoNumericoRamifica(this,j); 
-					listaHijos.add(estadoHijo);
-				}
-				
-				
-					
-			}
-
-		return listaHijos;
 	}
 
 	@Override
 	public boolean solucion() {
-		if (isSol() && ??)
+		// ya estan rellenos todos los huecos y se validan todas las operaciones
+		// (las que queden por evaluar) tanto por filas como por columnas.
+		if (isSol() && compruebaFilas() && compruebaColumna())
 			return true;
 		else
 			return false;
